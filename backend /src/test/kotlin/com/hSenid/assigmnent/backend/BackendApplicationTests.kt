f7c98b21b4be0f65ccdf0f846ef0701104e7dd41
Nipun Nirman <164348907+nipunnirman@users.noreply.github.com>
2026-03-27 10:11:30 +0530

    create springinizializer file.
    install 4 dependencis
    Spring Web WEB
    Build web, including RESTful, applications using Spring MVC. Uses Apache
    Tomcat as the default embedded container.
    Spring Data JPA
    SQL
    Persist data in SQL stores with Java Persistence API using Spring Data and Hibernate.
    H2 Database
    SQL
    Provides a fast in-memory database that supports JDBC API and R2DBC access, with a small (2mb) footprint. Supports embedded and server modes as well as a browser based console application.
    Validation NO
    Bean Validation with Hibernate validator.

---
+++ backend /src/test/kotlin/com/hSenid/assigmnent/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hSenid.assigmnent.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
